47b582dd15dd1ea1bfa75e87fdd4490d95d1d46a
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-01-18 14:50:34 +0100

    Exo-133

--- index.php
+++ index.php
@@ -3,45 +3,52 @@
 // Premiere ligne
 $a = 50;
 $b = 10;
-
-       >    {
+ if ($a > $b);  {
     echo "Hello World!";
 }
 
 //Deuxieme ligne
-    {
+   if ($a != $b) {
     echo "<br><br>Hello World2";
 }
 
 //Troisieme ligne
-($a==b) {
+if ($a === $b) {
     echo "<br><br>Oui";
 }
 
-{
+else{
     echo "<br><br>Non";
 }
 
 
 //Quatrieme ligne
 echo"<br><br>";
- ($a == $b) {
+ if ($a === $b) {
     echo "1";
-}  ($a > $b) {
+}
+ elseif ($a > $b) {
     echo "2";
 }
-{
+else{
     echo "3";
 }
 
 
 //Cinquieme ligne
 echo"<br><br>";
+$color = 5;
+$red = 5;
+$green = 6;
 
- ($color) {
-    "red":
+
+ if ($color === $red) {
     echo "Hello";
-   "green":
-    echo "Welcome";
 }
+ elseif ($color === $green){
+     echo "Welcome";
+ }
+ else{
+     echo "color ne vaux pas red ou grenn";
+ }
 
